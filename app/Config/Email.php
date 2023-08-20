94f3d1eeda8ad2df8d4b96641969089fd5d81b45
Daniel W Brick III <lucefin@gmail.com>
2023-08-20 15:58:03 -0400

    Added CI Shield with default config.

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'admin@projectbacklog.com';
+    public string $fromName   = 'Administrator';
     public string $recipients = '';
 
     /**
